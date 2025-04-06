659600e1fce1ef8bd560f203bf6d84b9dd4a7ae2
Ahmed-Fa1sal <DerRiese765@gmail.com>
2025-04-06 09:16:44 +0300

    Using date only instead of date and time when searching

--- Health_Informatics_System_Backend/Controllers/AppointmentController.cs
+++ Health_Informatics_System_Backend/Controllers/AppointmentController.cs
@@ -7,6 +7,8 @@ using Health_Informatics_System_Backend.Services;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using System.Globalization;
+
 
 namespace Health_Informatics_System_Backend.Controllers
 {
@@ -77,11 +79,17 @@ namespace Health_Informatics_System_Backend.Controllers
         
         [HttpGet("available-slots/{doctorId}")]
         public async Task<IActionResult> GetAvailableTimeSlots(
-            [FromRoute] int doctorId, 
-            [FromQuery] DateTime date)
+        [FromRoute] int doctorId, 
+        [FromQuery] string date)
         {
-            var slots = await _appointmentService.GetAvailableTimeSlots(doctorId, date);
-            return Ok(slots);
+        // Parse the date string (e.g., "2025-05-01") into a DateTime.
+        if (!DateTime.TryParseExact(date, "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime dateValue))
+        {
+         return BadRequest("Invalid date format. Please use yyyy-MM-dd.");
+        }
+
+         var slots = await _appointmentService.GetAvailableTimeSlots(doctorId, dateValue);
+         return Ok(slots);
         }
     }
 }
