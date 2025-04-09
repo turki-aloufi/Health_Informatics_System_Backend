2f6cac810bdf9bf0a2bd44ddef0cdb89065df82e
Mohammed alfaifi <mwmwx23x@gmail.com>
2025-04-09 10:13:44 +0300

    added user seed in database initializer

--- Health_Informatics_System_Backend/Program.cs
+++ Health_Informatics_System_Backend/Program.cs
@@ -55,6 +55,13 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    DbInitializer.Seed(dbContext);
+}
+
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
